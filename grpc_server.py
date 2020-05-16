3888996481412fc33f1ebe833eec625481e7c032
Priya Khadke <priyakhadke15@gmail.com>
2020-05-15 22:19:48 -0700

    Added service registry API calls(register,heartbeat) from GRPC server

--- grpc_server.py
+++ grpc_server.py
@@ -5,18 +5,29 @@ import file_server_pb2
 import file_server_pb2_grpc
 import time
 import os,logging,sys
+import requests
 
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 PROJECT_HOME = os.path.dirname(os.path.realpath(__file__))
 UPLOAD_FOLDER = '{}/uploads/'.format(PROJECT_HOME)
 CHUNK_SIZE = int(1024 * 1024 * 3.9) # 3.99MB
-
+serviceRegistry_url = 'http://0.0.0.0:5001'
+thisnodeAdd = {'ipaddress': sys.argv[1],
+               'port': sys.argv[2], 
+              }
+            
 # FileServiceServicer provides an implementation of the methods of the FileServer service.
 class FileServicer(file_server_pb2_grpc.FileServiceServicer):
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
         logging.info('initializing GRPC server')
+        try:
+            registerAPI = serviceRegistry_url+"/register"
+            x = requests.post(registerAPI, data = thisnodeAdd)
+
+        except Exception as e:
+            logging.warning('Failed to register: '+ str(e))
 
     def Upload(self, request, context):    
         try:
@@ -67,10 +78,17 @@ class FileServicer(file_server_pb2_grpc.FileServiceServicer):
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     file_server_pb2_grpc.add_FileServiceServicer_to_server(FileServicer(), server)
-    logging.info('Starting GRPC server on port :%s',str(sys.argv[1]))
-    port=str(sys.argv[1])
+    logging.info('Starting GRPC server on port :%s',str(sys.argv[2]))
+    port=str(sys.argv[2])
+    # change ipaddress logic here
     server.add_insecure_port('[::]:'+port)
     server.start()
+    starttime=time.time()
+    heartbeatAPI = serviceRegistry_url+"/heartbeat"
+    while True:
+        logging.info('heartbeat')
+        requests.put(heartbeatAPI, data = thisnodeAdd)
+        time.sleep(10)
     server.wait_for_termination()
 
 if __name__ == '__main__':
