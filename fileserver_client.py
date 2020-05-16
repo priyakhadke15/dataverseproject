e1d89714f39b11a4aaa5257a87b90f965d5e775e
Priya Khadke <priyakhadke15@gmail.com>
2020-05-15 22:20:57 -0700

    moved stub to grpc server connection to method

--- fileserver_client.py
+++ fileserver_client.py
@@ -14,12 +14,7 @@ class Client:
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
         logging.info("initializing GRPC client")
-        # Move the host selection logic next 2 lines to Consistent Hash algorithm
-        ports = self._getAllNodes()
-        portNumber=random.choice(ports)
-        channel = grpc.insecure_channel('localhost:'+portNumber)
-        self.stub = file_server_pb2_grpc.FileServiceStub(channel)
-
+        
     def _byteStream(self,fileHandle):
         while True:
             chunk = fileHandle.read(CHUNK_SIZE)
@@ -29,24 +24,33 @@ class Client:
             yield file_server_pb2.Chunk(chunk=chunk)
 
     def upload(self, uploadedFile):
-        logging.info("within GRPC client upload")        
-        chunks_generator = self._byteStream(uploadedFile)
-        metadata = (('filename', uploadedFile.filename),)
-        response = self.stub.Upload(chunks_generator, metadata=metadata)
-        return response.success
+        
+        logging.info("within GRPC client upload")  
+        try:
+            stub=self._connect()
+            chunks_generator = self._byteStream(uploadedFile)
+            metadata = (('filename', uploadedFile.filename),)
+            response = stub.Upload(chunks_generator, metadata=metadata)
+            return response.success
+        except Exception as e:
+            logging.warning("%s",str(e))
 
     def download(self, filename):
-        logging.info("within GRPC client download")   
-        response = self.stub.Download(file_server_pb2.Name(name=filename))
-        fileHandle = open(os.path.join(DOWNLOAD_FOLDER, filename), "wb")
-        if fileHandle is not None:
-            for chunk in response:
-                fileHandle.write(chunk.chunk)
-            fileHandle.close()
-            logging.info("Exiting GRPC client download")
-            return True
-        logging.warning("GRPC client fail download")    
-        return False
+        logging.info("within GRPC client download")
+        try:
+            stub=self._connect()
+            response = stub.Download(file_server_pb2.Name(name=filename))
+            fileHandle = open(os.path.join(DOWNLOAD_FOLDER, filename), "wb")
+            if fileHandle is not None:
+                for chunk in response:
+                    fileHandle.write(chunk.chunk)
+                fileHandle.close()
+                logging.info("Exiting GRPC client download")
+                return True
+            logging.warning("GRPC client fail download")    
+            return False
+        except Exception as e:
+            logging.warning("%s",str(e))  
     
     # Gets the list of GRPC Servers ports running as
     def _getAllNodes(self):
@@ -60,3 +64,12 @@ class Client:
             logging.info("GRPC servers on port %s",str(temp[len(temp)-1]))
             i=i+1
         return portList
+    
+    # connect to given GRPC server
+    def _connect(self):
+        # Move the host selection logic next 2 lines to Consistent Hash algorithm
+        ports = self._getAllNodes()
+        portNumber=random.choice(ports)
+        channel = grpc.insecure_channel('localhost:'+portNumber)
+        stub = file_server_pb2_grpc.FileServiceStub(channel)
+        return stub
